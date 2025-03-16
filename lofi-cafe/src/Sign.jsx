1230bb1f7c1baecddae372a7dcdeec3c8c889445
Koffi Cup <72196885+COTE-Allan@users.noreply.github.com>
2025-03-16 18:50:41 +0100

    lofi cafe 0.1

--- lofi-cafe/src/Sign.jsx
+++ lofi-cafe/src/Sign.jsx
@@ -7,50 +7,52 @@ import { BiHome } from "react-icons/bi";
 
 function Sign() {
   return (
-    <div className="sign">
-      <img className="sign-img" src={SignIMG} alt="panneau suspendu" />
-      <div className="sign-content">
-        <span>Welcome to...</span>
-        <h1>Cielesis's lofi cafe</h1>
-        <span>Enjoy your stay !</span>
-        <div className="sign-content-socials">
-          <a
-            href="https://www.cielesis.fr"
-            target="_blank"
-            className="sign-content-socials-item"
-          >
-            <FaHome />
-          </a>
-          <a
-            href="https://www.behance.net/allancote1#"
-            target="_blank"
-            className="sign-content-socials-item"
-          >
-            <BsBehance />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/cielesis/"
-            target="_blank"
-            className="sign-content-socials-item"
-          >
-            <FaLinkedinIn />
-          </a>
-          <a
-            href="https://x.com/koffi_cup"
-            target="_blank"
-            className="sign-content-socials-item"
-          >
-            <BsTwitterX />
-          </a>
-          <a
-            href="https://www.instagram.com/koffi_cup/"
-            target="_blank"
-            className="sign-content-socials-item"
-          >
-            <BsInstagram />
-          </a>
+    <div className="sign-container">
+      <div className="sign">
+        <img className="sign-img" src={SignIMG} alt="panneau suspendu" />
+        <div className="sign-content">
+          <span>Welcome to...</span>
+          <h1>Cielesis's lofi cafe</h1>
+          <span>Enjoy your stay !</span>
+          <div className="sign-content-socials">
+            <a
+              href="https://www.cielesis.fr"
+              target="_blank"
+              className="sign-content-socials-item"
+            >
+              <FaHome />
+            </a>
+            <a
+              href="https://www.behance.net/allancote1#"
+              target="_blank"
+              className="sign-content-socials-item"
+            >
+              <BsBehance />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/cielesis/"
+              target="_blank"
+              className="sign-content-socials-item"
+            >
+              <FaLinkedinIn />
+            </a>
+            <a
+              href="https://x.com/koffi_cup"
+              target="_blank"
+              className="sign-content-socials-item"
+            >
+              <BsTwitterX />
+            </a>
+            <a
+              href="https://www.instagram.com/koffi_cup/"
+              target="_blank"
+              className="sign-content-socials-item"
+            >
+              <BsInstagram />
+            </a>
+          </div>
         </div>
-      </div>
+      </div>{" "}
     </div>
   );
 }
